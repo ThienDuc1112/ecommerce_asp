33017fc61dc07a7f21b3ad50bc6482df30a7e54b
Le Trung Tai <tailtgbd201817@fpt.edu.vn>
2024-04-15 01:07:15 +0700

    config authenticate for back-end

---
+++ FinalProject/Context/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using FinalProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinalProject.Context
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> option):base(option) 
+        { 
+
+        }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("users");
+            /*base.OnModelCreating(modelBuilder);*/
+        }
+    }
+}
---
+++ FinalProject/Controllers/UserController.cs
@@ -0,0 +1,57 @@
+﻿using FinalProject.Context;
+using FinalProject.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.DotNet.Scaffolding.Shared.Messaging;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinalProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly AppDbContext _authContext;
+        public UserController(AppDbContext authContext)
+        {
+            _authContext = authContext;
+        }
+
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> Authenticate([FromBody] User userObj)
+        {
+            if (userObj == null)
+            {
+                return BadRequest();             
+            }
+
+            var user = await _authContext.Users
+                .FirstOrDefaultAsync(x => x.UserName == userObj.UserName && x.Password == userObj.Password);
+            if (user == null)
+            {
+                return NotFound(new { Message = "User Not Found!" });
+            }
+
+            return Ok(new
+            {
+                Message = "Login Success!"
+            });
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterUser([FromBody] User userObj)
+        {
+            if(userObj == null)
+            {
+                return BadRequest();
+            }
+
+            await _authContext.Users.AddAsync(userObj);
+            await _authContext.SaveChangesAsync();
+            return Ok(new
+            {
+                Message = "User Registered!"
+            });
+        }
+    }
+}
